3fe7845fd2c4b580d344b85558f24d3c845dd9da
Patrik Muniak <patrik.muniak@gmail.com>
2024-06-04 20:11:26 +0100

    added filter

--- static/js/main.module.js
+++ static/js/main.module.js
@@ -8,7 +8,6 @@ req.send(null);
 const venue_sess = JSON.parse(req.responseText);
 
 var tBodyRef = document.getElementById('ven-sess');
-// console.log(venue_sess)
 
 
 
@@ -21,10 +20,10 @@ for (var i = 0; i < venue_sess.length; i++) {
         if (key == "date") {
             var dt = new Date(value * 1000)
             const dayNames = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-            var value = dayNames[dt.getDay()]+", "+dt.getDate()+"/"+dt.getMonth()+"/"+dt.getFullYear()
+            var value = dayNames[dt.getDay()]+", "+dt.getDate()+"/"+(dt.getMonth()+1)+"/"+dt.getFullYear()
         } else if (key == "start" || key == "end") {
             var ts = new Date(value * 1000*60)
-            var value = ts.getHours()+":"+(ts.getMinutes() < 10 ? '0' : '') + ts.getMinutes()
+            var value = ts.getUTCHours()+":"+(ts.getUTCMinutes() < 10 ? '0' : '') + ts.getMinutes()
         }
         var content = document.createTextNode(value)
         c.appendChild(content);
@@ -34,18 +33,53 @@ for (var i = 0; i < venue_sess.length; i++) {
 const startInput = document.getElementById('tbl-src-strt');
 const venueNameInput = document.getElementById('tbl-src-vn-nm')
 const dateInput = document.getElementById('tbl-src-dt')
-const endInput = document.getElementById('tbl-src-end')
+
+function filterDate(date, targetDate){
+    if (targetDate.length>0) {
+        const date1 = new Date(date*1000).toDateString()
+        const date2 = new Date(targetDate).toDateString()
+        return date1 == date2
+    } else {
+        return true
+    }
+
+}
+
+function filterVenue(rowVal, inputVal){
+    if (inputVal.length>0) {
+        return rowVal.startsWith(inputVal)
+    } else {
+        return true
+    }
+
+}
+
+function filterAfterTime(rowTime, inputTime){
+    
+    if (inputTime.length>0) {
+        const inpt = Number(inputTime)*60
+        console.log(rowTime, inpt, rowTime>=inpt)
+        return rowTime>=inpt
+    } else {
+        return true
+    }
+
+}
+
 
 function filterTable() {
-    const venueName = venueNameInput.value
-    const date = dateInput.value
+    const inputVenue = venueNameInput.value
+    const inputDate = dateInput.value
+    const inputStartTime = startInput.value
+
     const res = venue_sess.filter((row) => {
-        if (venueName.length>0) {
-            return row['venue_name'].startsWith(venueName)
-        } else {
-            return true
-        }
-        return false
+        console.log(row)
+        const isDate = filterDate(row['date'], inputDate)
+        const isVenue = filterVenue(row['venue_name'], inputVenue)
+        const afterTime = filterAfterTime(row['start'], inputStartTime)
+        const condition = [isDate, isVenue, afterTime]
+        return condition.every(v => v===true)
+        
     })
     var Table = document.getElementById('ven-sess');
     Table.innerHTML = "";
@@ -58,10 +92,10 @@ function filterTable() {
             if (key == "date") {
                 var dt = new Date(value * 1000)
                 const dayNames = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-                var value = dayNames[dt.getDay()]+", "+dt.getDate()+"/"+dt.getMonth()+"/"+dt.getFullYear()
+                var value = dayNames[dt.getDay()]+", "+dt.getDate()+"/"+(dt.getMonth()+1)+"/"+dt.getFullYear()
             } else if (key == "start" || key == "end") {
-                var ts = new Date(value * 1000*60)
-                var value = ts.getHours()+":"+(ts.getMinutes() < 10 ? '0' : '') + ts.getMinutes()
+                var ts = new Date(value * 1000 * 60)
+                var value = ts.getUTCHours()+":"+(ts.getUTCMinutes() < 10 ? '0' : '') + ts.getMinutes()
             }
             var content = document.createTextNode(value)
             c.appendChild(content);
@@ -69,4 +103,5 @@ function filterTable() {
     }
 }
 startInput.addEventListener("input", filterTable);
+dateInput.addEventListener("input", filterTable);
 venueNameInput.addEventListener("input", filterTable);
