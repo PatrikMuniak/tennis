e201e8af0c5dcf8669ae7401ba9dce6870f50dd9
Patrik Muniak <patrik.muniak@gmail.com>
2024-07-01 21:13:51 +0100

    map feature

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,13 @@ import time
 from config import venues_cfg
 
 
-def get_venue_sessions(venue_id):
+def get_venue_sessions(id):
+
     con = sqlite3.connect(DB_PATH)
     cur = con.cursor()
-    cur.execute('''select * from requests where venue_id=? order by rowid desc limit 1;''', (venue_id,) )
+    cur.execute('''select * from requests where venue_id=? order by rowid desc limit 1;''', (id,) )
     query_out = cur.fetchone()
-
+    print(query_out)
     venue_sessions = json.loads(query_out[2])
     venue_name = query_out[3]
     venue_id = query_out[1]
@@ -27,13 +28,13 @@ def get_venue_sessions(venue_id):
                 end = session.get("EndTime")
                 booking_url = venues_cfg.get_by_id(venue_id, "booking_url")["booking_url"].format(date=datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d'))
                 if name == "6"or name=="10":
-                    print()
                     if end - start > 60:
                         for i in range(((end-start)//60)):
                             start_sess = start +60*i
                             end_sess = start +60*(i+1)
                             assert end_sess<=end
                             sessions.append({
+                                "venue_id":venue_id,
                                 "venue_name":venue_name,
                                 "date":date,
                                 "court_name":court_name,
@@ -42,7 +43,12 @@ def get_venue_sessions(venue_id):
                                 "end":end_sess,
                                 "booking_url":booking_url})
                     else:
-                        sessions.append({"venue_name":venue_name,"date":date,"court_name":court_name,"name":name,
+                        sessions.append({
+                                        "venue_id":venue_id,
+                                        "venue_name":venue_name,
+                                         "date":date,
+                                         "court_name":court_name,
+                                         "name":name,
                                          "start":start,
                                          "end":end,
                                          "booking_url":booking_url})
@@ -51,3 +57,7 @@ def get_venue_sessions(venue_id):
 def get_venues_list():
 
     return venues_cfg.retrieve_params("venue_name", "venue_id")
+
+def get_venues_for_map():
+
+    return venues_cfg.retrieve_params("venue_name", "venue_id", "latlng")
